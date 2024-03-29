3241207eaa2f9d8d2cbe4cbb5da133e0081cfafe
Sorabh001 <158773135+Sorabh001@users.noreply.github.com>
2024-03-29 08:58:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,13 @@
-function hideDropdown() {
-        var dropdown = document.getElementById("optionsDropdown");
-        dropdown.style.display = "none";
-        var downIcon = document.getElementById("downIcon");
-        var forwardIcon = document.getElementById("forwardIcon");
-        downIcon.style.display = "inline";
-        forwardIcon.style.display = "none";
-}
-
-var dropdownLinks = document.querySelectorAll(".dropdown-content a");
-dropdownLinks.forEach(function(link) {
-        link.addEventListener("click", function() {
-                hideDropdown();
-        });
+document.getElementById("urlForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var inputUrl = document.getElementById("gdriveurl").value;
+    var modifiedUrl = inputUrl.replace("https://drive.google.com/file/d/", "https://drive.google.com/uc?export=download&id/");
+    modifiedUrl = modifiedUrl.replace("/view?usp=drivesdk", "");
+    document.getElementById("output").value = modifiedUrl;
 });
 
-document.getElementById("optionsButton").addEventListener("click", function() {
-        var dropdown = document.getElementById("optionsDropdown");
-        dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
-        var downIcon = document.getElementById("downIcon");
-        var forwardIcon = document.getElementById("forwardIcon");
-        downIcon.style.display = downIcon.style.display === "none" ? "inline" : "none";
-        forwardIcon.style.display = forwardIcon.style.display === "none" ? "inline" : "none";
-});
-document.getElementById("closeButton").addEventListener("click", function() {
-  var container = document.querySelector(".container");
-  if (container.classList.contains("hidden")) {
-    container.classList.remove("hidden");
-  } else {
-    container.classList.add("hidden");
-  }
+document.getElementById("output").addEventListener("click", function() {
+    this.select();
+    document.execCommand("copy");
+    alert("link copy to clipboard!");
 });
